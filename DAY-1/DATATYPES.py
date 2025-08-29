8344bc22a7e7fd03509320b68718345b025e5eba
Amrthes <amirthae2425@gmail.com>
2025-08-29 23:29:40 +0530

    First commit - added user input demo code

---
+++ DAY-1/DATATYPES.py
@@ -0,0 +1,16 @@
+a=int(input('Enter a number: '))
+print(type(a))
+
+b=float(input('Enter a floating point number: '))
+print(type(b))
+
+
+c=bool(input('Enter a boolean value: '))
+print(type(d)) 
+
+d=input('Enter a string: ')
+print(type(c))
+
+
+e=complex(input('Enter a complex value: '))
+print(type(e))
