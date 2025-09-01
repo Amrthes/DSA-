07caceb57af1d8503fa641bc6e2098a4bdef5d28
Amrthes <amirthae2425@gmail.com>
2025-09-01 09:08:49 +0530

    Initial commit

---
+++ DAY-9/armstrong.py
@@ -0,0 +1,14 @@
+num = int(input("Enter a number: "))
+temp = num
+digits = len(str(num))
+sum_of_powers = 0
+
+while temp > 0:
+    digit = temp % 10
+    sum_of_powers += digit ** digits
+    temp //= 10
+
+if num == sum_of_powers:
+    print(num, "is an Armstrong Number")
+else:
+    print(num, "is NOT an Armstrong Number")
