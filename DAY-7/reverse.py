b81135f4e059bb9487e67000784fa5b86eb4889d
Amrthes <amirthae2425@gmail.com>
2025-09-01 08:57:10 +0530

    Initial commit

---
+++ DAY-7/reverse.py
@@ -0,0 +1,10 @@
+num = int(input("Enter a number: "))
+reverse = 0
+temp = num
+
+while temp > 0:
+    digit = temp % 10          
+    reverse = reverse * 10 + digit
+    temp //= 10               
+
+print("Reversed number:", reverse)
